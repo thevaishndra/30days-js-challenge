a133b76180103b1b6d11aaf0713b6e865226a771
thevaishndra <chandrav306m@gmail.com>
2024-07-13 19:19:45 +0530

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,39 @@
+//Variable declaration
+var number = 19;
+// console.log(number);
+
+let string = "Feeling ill today!";
+// console.log(string);
+
+//Constant declaration
+const isTrue = true;
+// console.log(isTrue);
+
+//Data Types
+let a = 56;
+let b = "Going to sleep";
+let c = false;
+let d ={
+    sky: "blue",
+    marigold: "orange"
+};
+let e = ["mercury", "venus", "earth", "mars", "jupiter"];
+
+ console.log(a, typeof(a));
+ console.log(b, typeof(b));
+ console.log(c, typeof(c));
+ console.log(d, typeof(d));
+ console.log(e, typeof(e));//array is special objects
+
+//Reassigning Variables
+let initialValue = 67;
+// let initialValue = 68; -> this is not allowed
+// console.log(initialValue);
+initialValue = 70;
+// console.log(initialValue);
+//we can reassign values using let
+
+//Understanding Const
+const iniVal = 42;
+// iniVal = 50;
+//error, const can't be reassigned
